7da77b92e067a7470d7668ec5cd1f10664556861
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-03-09 16:43:00 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -3,7 +3,9 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename T>
-T multiples (T& sum, T x, T n)
+T multiples (T& sum, T x, int n)
 {
-  return x * n;
+  for (int i = 1 ; i <= n ; i++) {
+    sum += i * x;
+  }
 }
